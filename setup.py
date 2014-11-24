67935564a7e404783c5add015fe2b5ce0b1a591e
facelessuser <faceless.shop@gmail.com>
2014-11-23 19:19:26 -0700

    Update setup

--- setup.py
+++ setup.py
@@ -16,33 +16,37 @@ https://github.com/facelessuser/PyMdown.
 '''
 
 setup(
-    name='pymdown',
-
+    name='PyMdown',
     version='0.7.0',
-
     keywords='markdown html batch',
     description='CLI Markdown batch converter/previewer.',
     long_description=LONG_DESC,
-
     author='Isaac Muse',
     author_email='Isaac.Muse [at] gmail.com',
-
     url='https://github.com/facelessuser/PyMdown',
-
     packages=find_packages(exclude=['pyinstaller*']),
-    install_requires=['Markdown>=2.5.2', 'Pygments>=2.0.1'],
-
+    install_requires=[
+        'Markdown>=2.5.2',
+        'Pygments>=2.0.1',
+        'pymdown-extensions>=1.0.0',
+        'pymdown-lexers>=1.0.0',
+        'pymdown-styles>=1.0.0'
+    ],
+    zip_safe=False,
+    dependency_links=[
+        'https://github.com/facelessuser/pymdown-extensions/archive/master.zip#egg=pymdown-extensions-1.0.0',
+        'https://github.com/facelessuser/pymdown-lexers/archive/master.zip#egg=pymdown-lexers-1.0.0',
+        'https://github.com/facelessuser/pymdown-styles/archive/master.zip#egg=pymdown-styles-1.0.0'
+    ],
     entry_points={
         'console_scripts': [
             'pymdown=pymdown:main',
             'pymdown%d.%d=pymdown:main' % sys.version_info[:2],
         ]
     },
-
     package_data={
         'pymdown.data': ['*.*']
     },
-
     license='MIT License',
     classifiers=[
         'Development Status :: 5 - Production/Stable',
@@ -59,6 +63,6 @@ setup(
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: Text Processing :: Filters',
-        'Topic :: Text Processing :: Markup :: HTML',
+        'Topic :: Text Processing :: Markup :: HTML'
     ]
 )
